259f6b0eee5240c797903b3b08a9eaabba2c34ee
weI <15127066784@163.com>
2022-08-23 11:10:54 +0800

    每天早上8点

--- main.py
+++ main.py
@@ -51,5 +51,6 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
-res = wm.send_template(my_user_id, template_id, data)
+res2 = wm.send_template(my_user_id, template_id, data)
 print(res)
+print(res2)
