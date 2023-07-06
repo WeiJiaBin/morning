b3581e66a29cbe5098fd21dc30d0a418598fb4dd
weI <wjb@jzdinfo.com>
2023-07-06 10:52:33 +0800

    qh

--- main.py
+++ main.py
@@ -46,12 +46,21 @@ def get_birthday():
     return (next - today).days
 
 
+# def get_words():
+#     words = requests.get("https://api.shadiao.pro/chp")
+#     if words.status_code != 200:
+#         return get_words()
+#     return words.json()['data']['text']
 def get_words():
-    words = requests.get("https://api.shadiao.pro/chp")
-    if words.status_code != 200:
-        return get_words()
-    return words.json()['data']['text']
+  # words = requests.get("https://api.shadiao.pro/chp")
+  # if words.status_code != 200:
+  #   return get_words()
+  # return words.json()['data']['text']
+  words = requests.get("https://apis.tianapi.com/saylove/index?key=6456a75fa758f057ff512acc785037a8")
 
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['result']['content']
 
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
