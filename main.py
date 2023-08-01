da559f69723ca889ccac148e03d3b112c54ac4cc
weI <wjb@jzdinfo.com>
2023-08-01 09:04:59 +0800

    更换情话

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def get_weather():
     location = '101300601'  #'101190107'
     url = "https://devapi.qweather.com/v7/weather/now?key=" + hefeng_key + "&location=" + location
     res = requests.get(url).json()
+    print(res)
     weather = res['now']
     weather['text'] = locationMap[location] + "-" + weather['text']
     return weather['text'], weather['feelsLike']
@@ -52,15 +53,15 @@ def get_birthday():
 #         return get_words()
 #     return words.json()['data']['text']
 def get_words():
-  # words = requests.get("https://api.shadiao.pro/chp")
-  # if words.status_code != 200:
-  #   return get_words()
-  # return words.json()['data']['text']
-  words = requests.get("https://apis.tianapi.com/saylove/index?key=6456a75fa758f057ff512acc785037a8")
-
+  words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
-  return words.json()['result']['content']
+  return words.json()['data']['text']
+  # words = requests.get("https://apis.tianapi.com/saylove/index?key=6456a75fa758f057ff512acc785037a8")
+  #
+  # if words.status_code != 200:
+  #   return get_words()
+  # return words.json()['result']['content']
 
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
